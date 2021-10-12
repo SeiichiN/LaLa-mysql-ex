263935e5cd2ab90b40591cc7d42b37d55f460811
SeiichiN <billie175@gmail.com>
2021-10-12 16:49:06 +0900

    10/12 after

--- student/student_view.sql
+++ student/student_view.sql
@@ -4,10 +4,12 @@ SELECT
     p.name     AS 名前,
     g.gname    AS 性別,
     p.birthday AS 誕生日,
-    p.state    AS 出身,
+    s.name     AS 出身,
     c.c_name   AS コース
   FROM person p
   INNER JOIN gender g
     ON p.gender = g.gid
   INNER JOIN course c
-    ON p.c_code = c.c_id;
+    ON p.c_code = c.c_id
+  INNER JOIN state s
+    ON p.state = s.code;
