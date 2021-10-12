9c0368d01f4a4af0672f22585e63cfc02bca58f6
SeiichiN <billie175@gmail.com>
2021-10-12 09:12:29 +0900

    gender.sqlなどを作成

---
+++ student/inner_join.sql
@@ -0,0 +1,13 @@
+CREATE VIEW student_view AS
+SELECT
+    p.id       AS ID,
+    p.name     AS 名前,
+    g.gname    AS 性別,
+    p.birthday AS 誕生日,
+    p.state    AS 出身,
+    c.c_name   AS コース
+  FROM person p
+  INNER JOIN gender g
+    ON p.gender = g.gid
+  INNER JOIN course c
+    ON p.c_code = c.c_id;
